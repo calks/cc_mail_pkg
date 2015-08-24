1277a518dc00fe8dcc1c0e9c0b280626a70e3b8f
Alex <none@none>
2015-08-24 13:48:16 +0600

    Добавлен скрипт создания таблиц

---
+++ deploy_db_scripts/2015-08-24_create_mail_tables.sql
@@ -0,0 +1,15 @@
+﻿SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `email_log`;
+CREATE TABLE  `email_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` datetime DEFAULT NULL,
+  `email_from` varchar(255) DEFAULT NULL,
+  `email_to` varchar(255) DEFAULT NULL,
+  `email_subject` varchar(255) DEFAULT NULL,
+  `email_body` text,
+  `succeed` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS=1;
