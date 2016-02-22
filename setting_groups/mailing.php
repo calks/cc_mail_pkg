c91e8e319c1d80fcd964ff630b7f1652c68dc4f2
Alex <none@none>
2016-02-22 17:24:54 +0600

    Переход на gettext: названия настроек теперь на английском

--- setting_groups/mailing.php
+++ setting_groups/mailing.php
@@ -4,7 +4,7 @@
 		
 		public function getGroupNames() {
 			return array(
-				'mailer' => 'Отправка Email'
+				'mailer' => 'Mail sending'
 			);
 		}
 		
@@ -12,33 +12,33 @@
 			$out = array(
 				'default_from_name' => array(
 					'type' => 'string',
-					'displayed_name' => 'Имя по умолчанию в поле FROM'					
+					'displayed_name' => 'Sender default name (FROM)'
 				),
 			
 				'default_from_email' => array(
 					'type' => 'string',
-					'displayed_name' => 'Email по умолчанию в поле FROM'					
+					'displayed_name' => 'Sender default email (FROM)'
 				),
 			
 				'transport' => array(
 					'type' => 'select',
-					'displayed_name' => 'Способ отправки',
+					'displayed_name' => 'Send method',
 					'value' => 'mail',
 					'constraints' => array(
 						'options' => array(
 							'smtp' => 'SMTP', 
-							'mail' => 'функция mail()', 
+							'mail' => 'mail() function', 
 							'sendmail' => 'Sendmail'
 						)
 					)
 				),
 				'smtp_host' => array(
 					'type' => 'string',
-					'displayed_name' => 'SMTP: хост'					
+					'displayed_name' => 'SMTP: host'					
 				),
 				'smtp_port' => array(
 					'type' => 'integer',
-					'displayed_name' => 'SMTP: порт',
+					'displayed_name' => 'SMTP: port',
 					'constraints' => array(
 						'min' => 1,
 						'max' => 65535				
@@ -46,22 +46,22 @@
 				),
 				'smtp_auth' => array(
 					'type' => 'checkbox',
-					'displayed_name' => 'SMTP: использовать авторизацию'					
+					'displayed_name' => 'SMTP: use authirization'					
 				),
 				'smtp_username' => array(
 					'type' => 'string',
-					'displayed_name' => 'SMTP: имя пользователя'					
+					'displayed_name' => 'SMTP: username'					
 				),
 				'smtp_password' => array(
 					'type' => 'string',
-					'displayed_name' => 'SMTP: пароль'					
+					'displayed_name' => 'SMTP: pasword'					
 				),
 				'smtp_secure' => array(
 					'type' => 'select',
-					'displayed_name' => 'SMTP: тип шифрования',
+					'displayed_name' => 'SMTP: security',
 					'constraints' => array(
 						'options' => array(
-							null => 'не выбран', 
+							null => 'not set', 
 							'ssl' => 'ssl',
 							'tls' => 'tls'
 						)
@@ -69,7 +69,7 @@
 				),
 				'enable_logging' => array(
 					'type' => 'checkbox',
-					'displayed_name' => 'Вести лог отправки'					
+					'displayed_name' => 'Enable email logging'					
 				)
 				
 			);
